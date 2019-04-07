f5e373bb9e75d65ac38abae8625f04493d3371db
Enrique <user1@user1s-MacBook-Pro.local>
2019-04-07 04:42:51 -0500

    format code

---
+++ app/src/main/java/mobrsg/android/redsinergia/gallerydemo/Constants.java
@@ -0,0 +1,7 @@
+package mobrsg.android.redsinergia.gallerydemo;
+
+public interface Constants {
+
+    String KEY = "ccbb1f20d6361b9583465f467321aaab680c3c97b5d7d24b07cf974fb21107cd";
+    String BASE_URL = "https://api.unsplash.com/";
+}
--- app/src/main/java/mobrsg/android/redsinergia/gallerydemo/api/RetrofitAdapter.java
+++ app/src/main/java/mobrsg/android/redsinergia/gallerydemo/api/RetrofitAdapter.java
@@ -7,6 +7,9 @@ import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static mobrsg.android.redsinergia.gallerydemo.Constants.BASE_URL;
+import static mobrsg.android.redsinergia.gallerydemo.Constants.KEY;
+
 /*
  * Crea la instancia de retrofit para despues crear el request a la API
  * el addInterceptor sirve para agregar header a tu peticionen caso de que lo requiera como
@@ -16,30 +19,21 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitAdapter {
 
-    private final String KEY = "ccbb1f20d6361b9583465f467321aaab680c3c97b5d7d24b07cf974fb21107cd";
-    private String BASE_URL = "https://api.unsplash.com/";
-
-    private  Retrofit retrofit;
-    private  OkHttpClient client;
-
     public Retrofit getInstance() {
-        client = new OkHttpClient.Builder()
+        OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(chain -> {
                     Request request = chain.request();
                     request = request.newBuilder()
                             .addHeader("Authorization", "Client-ID " + KEY)
                             .build();
                     return chain.proceed(request);
-                })
-                .build();
+                }).build();
 
-        retrofit = new Retrofit.Builder()
+        return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
-        return retrofit;
     }
 
 }
